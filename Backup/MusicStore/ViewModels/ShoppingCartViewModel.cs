44cd5b11ed49c5a315dad6856eb06f179e32506e
Basil N <basil.nyachogo@gmail.com>
2012-09-07 21:01:49 -0400

    stuff

---
+++ Backup/MusicStore/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using MusicStore.Models;
+
+namespace MusicStore.ViewModels {
+	public class ShoppingCartViewModel {
+		public List<Cart> CartItems { get; set; }
+		public decimal CartTotal { get; set; }
+	}
+}
